f29cb94c107387ec5c34359a722de4f86c744bfa
JC <jessica.l.conrad@gmail.com>
2025-03-12 14:52:18 +0100

    stored procedures finished ... I think

---
+++ EpreuveCSharp/DatabaseT4D/Stored Procedures/SP_Emprunt_Create.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[SP_Emprunt_Create]
+	@date_lent DATE NULL,
+	@gamecopy_id UNIQUEIDENTIFIER,
+	@borrower_id UNIQUEIDENTIFIER,
+	@lender_id UNIQUEIDENTIFIER
+AS
+BEGIN 
+
+    IF @date_lent IS NULL
+    BEGIN
+        SET @date_lent = CAST(GETDATE() AS DATE)
+    END
+
+	INSERT INTO [Emprunt] ([Date_Lent],[GameCopy_Id],[Borrower_Id],[Lender_Id])
+	OUTPUT [inserted].[Emprunt_Id]
+	VALUES (@date_lent, @gamecopy_id, @borrower_id, @lender_id)
+END
---
+++ EpreuveCSharp/DatabaseT4D/Stored Procedures/SP_Emprunt_Update.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SP_Emprunt_Update]
+	@emprunt_id INT,
+	@date_returned DATE NULL,
+	@eval_by_lender INT NULL,
+	@eval_by_borrower INT NULL
+	
+AS
+BEGIN
+	UPDATE [Emprunt]
+		SET	[Date_Returned] = @date_returned,
+			[Eval_By_Lender] = @eval_by_lender,
+			[Eval_By_Borrower] = @eval_by_borrower
+		WHERE [Emprunt_Id] = @emprunt_id
+END
