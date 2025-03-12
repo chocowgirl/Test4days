39d3f592f9976fb7a8cfac88a6e14c3fd21bb7cc
JC <jessica.l.conrad@gmail.com>
2025-03-12 09:57:32 +0100

    testing checks in post-deployment scripts and populating a minimal amount of information

--- EpreuveCSharp/DatabaseT4D/obj/Debug/postdeploy.sql
+++ EpreuveCSharp/DatabaseT4D/obj/Debug/postdeploy.sql
@@ -15,4 +15,11 @@ INSERT INTO [Tag] ([Tag_Name]) VALUES ('Dice');
 INSERT INTO [Tag] ([Tag_Name]) VALUES ('Cards');
 INSERT INTO [Tag] ([Tag_Name]) VALUES ('Board');
 INSERT INTO [Tag] ([Tag_Name]) VALUES ('Money');
+INSERT INTO [User] ([User_Id],[Email], [Password], [Pseudo], [Salt]) VALUES ('916a7414-b651-4662-a047-9bd359b98a98', 'john.doe@hotmail.com', CONVERT(VARBINARY, 'Test1234='), 'PseudoJohn', CAST('e3f47b5a-2d6c-4f90-9b2e-1a8a4e6d7c3f' AS UNIQUEIDENTIFIER));
+INSERT INTO [User] ([User_Id],[Email], [Password], [Pseudo], [Salt]) VALUES ('7cfc15b0-5391-44f2-9b83-c8da2ab13aa3','jane.doe@hotmail.com', CONVERT(VARBINARY, 'Test1234='), 'PseudoJane', CAST('3f5d6c8a-7e12-4b9a-bf25-9a3d2eab4f68' AS UNIQUEIDENTIFIER));
+INSERT INTO [Jeu] ([Title], [Description], [Min_Age], [Max_Age], [Min_Players], [Max_Players], [Duration_Mins]) VALUES ('Solitaire', 'A game played solo against a deck of cards', 6, 120, 1, 1, 60);
+INSERT INTO [Jeu] ([Title], [Description], [Min_Age], [Max_Age], [Min_Players], [Max_Players]) VALUES ('Battleship', 'The game where you try to sink the fleet of your opponent', 5, 129, 2, 2);
+INSERT INTO [GameCopy] ([Jeu_Id], [UserOwner_Id], [Copy_Condition]) VALUES (1, '916a7414-b651-4662-a047-9bd359b98a98', 'incomplete');
+INSERT INTO [GameCopy] ([Jeu_Id], [UserOwner_Id], [Copy_Condition]) VALUES (2, '7cfc15b0-5391-44f2-9b83-c8da2ab13aa3', 'good');
+
 GO
